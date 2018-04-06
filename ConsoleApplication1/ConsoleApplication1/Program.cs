f18b04490aaa9d11740880e26525eb17a76a571c
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-06 23:07:38 +0200

    First commit of the first Console program from Sololearn introduction to C#

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int = 89;
+            Console.WriteLine("Hello Thierry How was your day?");
+            Console.Write(x - 89);
+            Console.WriteLine(""); /* This only uses the automatic line return of WriteLine*/
+        }
+    }
+}
