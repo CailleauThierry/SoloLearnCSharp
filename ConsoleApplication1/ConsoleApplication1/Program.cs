5a6ee15a7b3614bda33d1ece32f9af2e28c33ba9
CailleauThierry <thierrycailleau@yahoo.fr>
2018-04-07 23:11:57 +0200

    if else with console entry

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,14 +10,22 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int x = 8;
-            int y = 3;
+            int x;
+            int y;
+            Console.WriteLine("Please enter a vlaue for x:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a vlaue for y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
             if (x>y)
             {
                 Console.WriteLine(" x is: {0}\n y is: {1}", x, y);
                 Console.WriteLine(" x is greater than y by: {0}", (x-y));
             }
-
+            else
+            {
+                Console.WriteLine(" x is: {0}", x);
+            }
         }
     }
 }
